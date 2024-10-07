a4032b153eaf026bc7ec1becf3ab467a99c0019f
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-10-07 17:18:11 +0600

    ok project

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,13 +2,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
-// Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://recipe-sharing-community-server.vercel.app/api",
-    // baseUrl: "http://localhost:5000/api",
-    // credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
